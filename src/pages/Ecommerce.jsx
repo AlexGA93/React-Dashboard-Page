0a3606713d097c19e31dfae0b7bf3b2523cc982f
AlexGA93 <alexgimat@hotmail.com>
2022-11-09 17:10:26 +0100

    charts

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 // react icons
 import { BsCurrencyDollar } from "react-icons/bs";
 import { GoPrimitiveDot } from "react-icons/go";
