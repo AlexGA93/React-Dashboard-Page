fc796ece5f4216220f4371166e6cc1937d09b220
AlexGA93 <alexgimat@hotmail.com>
2022-11-09 10:32:59 +0100

    Color Picker Component

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+
+// components
+import { Header, CustomColorPicker } from '../components';
 
 const ColorPicker = () => {
   return (
-    <div>ColorPicker</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="App" title="Color Picker" />
+    <div className="text-center">
+      <div id="preview" />
+      <div className="flex justify-center items-center gap-20 flex-wrap">
+        <div>
+          <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+          <CustomColorPicker id="inline-palette" mode="Palette" />
+        </div>
+        <div>
+          <p className="text-2xl font-semibold mt-2 mb-4">Inline Picker</p>
+          <CustomColorPicker id="inline-picker" mode="Picker" />
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
 
