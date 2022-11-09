dd6ec88aab7917091007e881d3c9564137ea79c0
AlexGA93 <alexgimat@hotmail.com>
2022-11-09 09:42:37 +0100

    Text Editor Component

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+
+// editor
+import {
+  HtmlEditor,
+  Image,
+  Inject,
+  Link,
+  QuickToolbar,
+  RichTextEditorComponent,
+  Toolbar
+} from '@syncfusion/ej2-react-richtexteditor';
+
+// data
+import { EditorData } from '../data/dummy';
+
+// Header
+import {Header} from '../components';
 
 const Editor = () => {
   return (
-    <div>Editor</div>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Editor" />
+      {/* editor */}
+      <RichTextEditorComponent>
+        
+        {/* External Data */}
+        <EditorData />
+
+        <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
+      </RichTextEditorComponent>
+    </div>
   )
 }
 
